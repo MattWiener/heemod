ea800019113402524837b098b00e8f36d976aee7
Wiener, Matthew <Matthew_Wiener@merck.com>
2017-06-21 08:30:18 +0200

    add tests for prepare_plot_data_from_fit_tibble

--- tests/testthat/test_survival_fit_output.R
+++ tests/testthat/test_survival_fit_output.R
@@ -15,6 +15,49 @@ test_that("pad_matrix works",
           }
           )
 
+test_that("prepare plot data from fit tibble",
+          {
+            location <- system.file("tabular/surv", package = "heemod")
+            ok_surv_info <- 
+              heemod:::read_file(system.file("tabular/surv/survival_info.csv", 
+                                             package = "heemod"))
+            full_these_fits <- 
+              heemod:::survival_from_data(location = location,
+                                          survival_specs = ok_surv_info,
+                                          dists = c("exp", "weibull"),
+                                          save_fits = FALSE) 
+            these_fits <- dplyr::filter_(full_these_fits,
+                                         ~treatment != "all")
+            aa <- prepare_plot_data_from_fit_tibble(these_fits[1:3, ], B_ci = 0)
+            expect_equal
+            expect_equal(aa$est[seq(from = 0, to = 2000, by = 50)],
+                         c(0.89900388, 0.80820798, 0.72658211, 0.65320014, 
+                           0.58722946, 0.52792156, 0.47460354, 0.84, 0.66, 
+                           0.6, 0.58, 0.56, 0.54, 0.84328836, 0.75056722, 
+                           0.68373791, 0.63030209, 0.58551426, 0.54691921, 
+                           0.51303849, 0.08304498, 0.18951291, 0.29598084, 
+                           0.40244876, 0.50891669, 0.61538462, 0.72185254, 
+                           0.15082289, 0.32850407, 0.51082562, 0.54472718, 
+                           0.5798185, 0.61618614, 0.13827727, 0.26400002, 
+                           0.36090194, 0.44441258, 0.51957942, 0.58885222, 
+                           0.65365155)
+                         )
+            expect_equal(aa$dist,
+                         rep(rep(c("exp", "km","weibull"), 
+                                 c(350, 311, 350)),
+                             2)
+            )
+            expect_equal(unique(aa$fn[1:1011]), "survival")
+            expect_equal(unique(aa$fn[1011 + 1:1011]), "cumulative hazard")
+            expect_error(prepare_plot_data_from_fit_tibble(these_fits$fit, B_ci = 0),
+                         "fit_tib should be a tibble")
+            expect_error(prepare_plot_data_from_fit_tibble(these_fits[, -1]),
+                         "fit_tib is missing column with name: type",
+                         fixed = TRUE)
+            
+          }
+          )
+
 test_that("get_component works",
           {
             expect_identical(
